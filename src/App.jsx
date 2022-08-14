c4cb611304115ec16fe0ece9ed0f50eec1bc4a4a
Marcos L <marcosrlj.p@gmail.com>
2022-08-13 23:40:09 -0300

    estrutura da aplicacao
    css global

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 
+import './global.css'
+
 function App() {
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export const Header = () => {
   return (
     <header className={styles.header}>
-    <strong>Ignite Feed</strong>
+      <strong>Ignite Feed</strong>
     </header>
   )
 }
